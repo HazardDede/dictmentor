7c46cbd3fbf4b1ee2186c62d398d16281eef9a2e
Dennis Muth <d.muth@gmx.net>
2020-10-15 10:44:55 +0200

    Adds test case with nested environment default

--- tests/extensions/test_environment.py
+++ tests/extensions/test_environment.py
@@ -11,6 +11,14 @@ def test_environment_with_inline_default():
     assert res == {'a': 1, 'file_path': 'my_file.local.cfg'}
 
 
+def test_environment_with_nested_inline_default():
+    jstr = '{"a": 1, "file_path": "my_file.{{env::ENVIRONMENT:={{env::DEFAULT}}.cfg}}"}'
+    with modified_environ('ENVIRONMENT', DEFAULT='the_default'):
+        res = DictMentor().bind(Environment()).load_yaml(jstr)
+
+    assert res == {'a': 1, 'file_path': 'my_file.the_default.cfg'}
+
+
 def test_environment_with_multiple_patterns():
     jstr = '{"a": 1, "file_path": "{{env::A}}-{{env::B}}-{{env::C}}"}'
     with modified_environ(A='aval', B='bval', C='cval'):
